b60e9ee51994e99acfa1039804c1875cfed9a683
root <root@centos8.ocplab.local>
2023-07-20 14:53:03 +0000

    asdqwe

--- application-mainframe.py
+++ application-mainframe.py
@@ -1,5 +1,41 @@
-import ibm_db
-conn = ibm_db.connect("database_cos","userdb","QOPWEQW1203")
-import ibm_db
-ibm_db.connect("DATABASE=name;HOSTNAME=host;PORT=60000;PROTOCOL=TCPIP;UID=username;
-                PWD=password;", "", "")
+- text: |
+    CONNECTION_URI="db2://root:m42ploz2wd@google.com:5434/hellothere"
+  host: google.com
+  port: "5434"
+  username: root
+  password: m42ploz2wd
+  scheme: db2
+  database: hellothere
+  connection_uri: db2://root:m42ploz2wd@google.com:5434/hellothere
+
+- text: |
+    uri="ibm_db_sa://user:str0ngp4ss@google.com:3000/hellothere"
+  host: google.com
+  port: "3000"
+  username: user
+  password: str0ngp4ss
+  scheme: ibm_db_sa
+  database: hellothere
+  connection_uri: ibm_db_sa://user:str0ngp4ss@google.com:3000/hellothere
+
+# Test special characters in password
+- text: |
+    uri="ibm_db_sa://user:str0ngp@ss!@google.com:3000/hellothere"
+  host: google.com
+  port: "3000"
+  username: user
+  password: str0ngp@ss!
+  scheme: ibm_db_sa
+  database: hellothere
+  connection_uri: ibm_db_sa://user:str0ngp@ss!@google.com:3000/hellothere
+
+# Test detection in md files
+- text: |
+    uri="ibm_db_sa://user:str0ngp@ss!@google.com:3000/hellothere"
+  host: google.com
+  port: "3000"
+  username: user
+  password: str0ngp@ss!
+  scheme: ibm_db_sa
+  database: hellothere
+  connection_uri: ibm_db_sa://user:str0ngp@ss!@google.com:3000/hellothere
