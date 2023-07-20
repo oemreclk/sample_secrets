349b3954b5b98e9b4035f74ee43a0aea3edf75aa
root <root@centos8.ocplab.local>
2023-07-20 14:43:14 +0000

    asdasd

---
+++ application-mainframe.py
@@ -0,0 +1,10 @@
+#Replace the placeholder values with your actual Db2 hostname, username, and password:
+dsn_hostname = "2d46b6b4-cbf6-40eb-bbce-6251e6ba0300.bs2io90l08kqb1od8lcg.databases.appdomain.cloud" # e.g.: "54a2f15b-5c0f-46df-8954-7e38e612c2bd.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud"
+dsn_uid = "vjd29721"        # e.g. "abc12345"
+dsn_pwd = "6TTgx8MRBzT45o3q"      # e.g. "7dBZ3wWt9XN6$o0J"
+
+dsn_driver = "{IBM DB2 ODBC DRIVER}"
+dsn_database = "BLUDB"            # e.g. "BLUDB"
+dsn_port = "32328"                # e.g. "32733" 
+dsn_protocol = "TCPIP"            # i.e. "TCPIP"
+dsn_security = "SSL"              #i.e. "SSL"
