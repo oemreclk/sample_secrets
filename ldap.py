c14a73e1e0d006c7e84c2adb3ce0f965a8312ee4
Osman Emre Celik <emrecelikk@windowslive.com>
2023-07-21 09:54:19 +0300

    aasds

---
+++ ldap.py
@@ -0,0 +1,31 @@
+- text: |
+    ldap_uri = ldaps://company.beta.com
+    ldap_bind_dn = a_ldap_user_01@company.beta.com
+    ldap_pass = "k%udk423u4%P8=H_"
+  host: company.beta.com
+  username: a_ldap_user_01@company.beta.com
+  password: k%udk423u4%P8=H_
+
+- text: |
+    ldap_server = ldaps://company.beta.com
+    ldap_user = a_ldap_user_01
+    ldap_pwd = "k%udk423u4%P8=H_"
+  host: company.beta.com
+  username: a_ldap_user_01
+  password: k%udk423u4%P8=H_
+
+- text: |
+    ldap_server = ldaps://company.beta.com:389
+    ldap_user = a_ldap_user_01
+    ldap_pwd = "k%udk423u4%P8=H_"
+  host: company.beta.com:389
+  username: a_ldap_user_01
+  password: k%udk423u4%P8=H_
+
+- text: |
+    ldap_server = 124.36.78.214:389
+    ldap_user = a_ldap_user_01
+    ldap_pwd = "k%udk423u4%P8=H_"
+  host: 124.36.78.214:389
+  username: a_ldap_user_01
+  password: k%udk423u4%P8=H_
