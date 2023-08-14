591dcaca4768af655105afff83d816051c38247d
oemreclk <45369508+oemreclk@users.noreply.github.com>
2023-08-14 17:08:28 +0300

    Update redis.py

--- redis.py
+++ redis.py
@@ -1,42 +1,57 @@
-- text: |
-    CONECTION_URI="redis://root:m42ploz2wd@google.com:5434/thegift"
-  host: google.com
-  port: "5434"
-  username: root
-  password: m42ploz2wd
-  scheme: redis
-  database: thegift
-  connection_uri: redis://root:m42ploz2wd@google.com:5434/thegift
-
-- text: |
-    CONECTION_URI="rediss://root:m42ploz2wd@google.com:5434/thegift"
-  host: google.com
-  port: "5434"
-  username: root
-  password: m42ploz2wd
-  scheme: rediss
-  database: thegift
-  connection_uri: rediss://root:m42ploz2wd@google.com:5434/thegift
-
-# Test special characters in password
-- text: |
-    CONECTION_URI="rediss://root:m42p!o@2wd@google.com:5434/thegift"
-  host: google.com
-  port: "5434"
-  username: root
-  password: m42p!o@2wd
-  scheme: rediss
-  database: thegift
-  connection_uri: rediss://root:m42p!o@2wd@google.com:5434/thegift
-
-# Test detection in md files
-- text: |
-    CONECTION_URI="rediss://root:m42p!o@2wd@google.com:5434/thegift"
-  host: google.com
-  port: "5434"
-  username: root
-  password: m42p!o@2wd
-  scheme: rediss
-  database: thegift
-  connection_uri: rediss://root:m42p!o@2wd@google.com:5434/thegift
-
+apiVersion: "logging.openshift.io/v1"
+kind: "ClusterLogging"
+metadata:
+  name: "instance"
+  namespace: "openshift-logging"
+spec:
+  managementState: "Managed"
+  logStore:
+    type: "elasticsearch"
+    retentionPolicy:
+      application:
+        maxAge: 1d
+      infra:
+        maxAge: 7d
+      audit:
+        maxAge: 7d
+    elasticsearch:
+      nodeCount: 3
+      nodeSelector:
+        node-role.kubernetes.io/infra: ''
+      tolerations:
+      - effect: NoSchedule
+        key: node-role.kubernetes.io/infra
+        value: reserved
+      - effect: NoExecute
+        key: node-role.kubernetes.io/infra
+        value: reserved
+      storage:
+        storageClassName: "<storage_class_name>" 
+        size: 200G
+      resources:
+        limits:
+          memory: "16Gi"
+        requests:
+          memory: "16Gi"
+      proxy:
+        resources:
+          limits:
+            memory: 256Mi
+          requests:
+             memory: 256Mi
+      redundancyPolicy: "SingleRedundancy"
+  visualization:
+    type: "kibana"
+    kibana:
+      nodeSelector:
+        node-role.kubernetes.io/infra: ''
+      tolerations:
+      - effect: NoSchedule
+        key: node-role.kubernetes.io/infra
+        value: reserved
+      - effect: NoExecute
+        key: node-role.kubernetes.io/infra
+        value: reserved
+      replicas: 1
+  collection:
+    type: fluentd
